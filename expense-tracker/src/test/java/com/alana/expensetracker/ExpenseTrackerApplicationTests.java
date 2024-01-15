b4cc73f324ad5a031b8158e89c44d8518de16b61
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-15 12:28:53 +0100

    first project for the week

---
+++ expense-tracker/src/test/java/com/alana/expensetracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alana.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

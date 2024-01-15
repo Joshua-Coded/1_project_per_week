3d1b3d3a1bdc652c1849e619a7d424fd685b1a57
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-15 16:19:10 +0100

    controller

--- expense-tracker/src/main/java/com/alana/expensetracker/controller/ExpenseController.java
+++ expense-tracker/src/main/java/com/alana/expensetracker/controller/ExpenseController.java
@@ -3,16 +3,15 @@ package com.alana.expensetracker.controller;
 import com.alana.expensetracker.dto.ExpenseDto;
 import com.alana.expensetracker.service.ExpenseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/expense")
@@ -31,6 +30,29 @@ public class ExpenseController {
 
         return ResponseEntity.created(location.toURI())
                 .build();
+    }
 
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void updateExpense(@RequestBody ExpenseDto expenseDto) {
+        expenseService.updatExpense(expense);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ExpenseDto> getAllExpenses() {
+        return expenseService.getAllExpenses();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ExpenseDto getExpense(@PathVariable String id){
+        return expenseService.getExpense(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteExpense(PathVariable String id){
+        expenseService.deleteExpense(id);
     }
 }

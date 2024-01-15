d56001487cc82d253fbf2ac0c41f97c9794be1f9
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-15 16:23:41 +0100

    dto

--- expense-tracker/src/main/java/com/alana/expensetracker/dto/ExpenseDto.java
+++ expense-tracker/src/main/java/com/alana/expensetracker/dto/ExpenseDto.java
@@ -1,4 +1,21 @@
 package com.alana.expensetracker.dto;
 
+
+import com.alana.expensetracker.model.ExpenseCategory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ExpenseDto {
+    private String id;
+    private String expenseName;
+    private ExpenseCategory expenseCategory;
+    private BigDecimal amount;
 }
